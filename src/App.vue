3c07d2458c5e847759954abcdfd6fd66b8f8d68a
Fineas Anton <fineas.anton@gmail.com>
2020-02-13 09:56:55 -0600

    added transitions

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,18 @@
         <h1 class="text-center">Super Quiz</h1>
       </div>
       <hr />
+      <br /><br />
       <div class="row">
         <div
           class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"
         >
-          <component
-            :is="mode"
-            @answered="answered($event)"
-            @confirmed="mode = 'question'"
-          ></component>
+          <transition name="slide-fade">
+            <component
+              :is="mode"
+              @answered="answered($event)"
+              @confirmed="mode = 'question'"
+            ></component>
+          </transition>
         </div>
       </div>
     </div>
@@ -48,4 +51,15 @@ export default {
 </script>
 
 <style>
+.slide-fade-enter-active {
+  transition: all 0.3s ease;
+}
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
 </style>
